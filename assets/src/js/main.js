366bfbf1ad6c8bae4bdbeb09604ed863e3895074
iftekher2108 <parvezkhan21082002@gmail.com>
2024-02-29 18:22:59 +0600

    merine ship project

---
+++ assets/src/js/main.js
@@ -0,0 +1,38 @@
+console.log($('.slick-slide'))
+console.log('iftekher mahmud pervez')
+console.log($('.slick-slide'))
+
+$(document).ready(function(){
+
+})
+
+$('.custom-slide').slick({
+//     slidesToShow: 1,
+//   slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed:3000,
+})
+
+$('.news-slide').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  // autoplay: false,
+  // infinite:false,
+  // autoplaySpeed: 1000,
+})
+
+$('.partner-slide').slick({
+        slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      infinite:true,
+      autoplaySpeed: 1000,
+    })
